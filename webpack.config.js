8ddfd39572d842a07c5cba85c6d373102cf4cf0d
Hiren Joshi <hirenj@gmail.com>
2019-06-20 22:07:04 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  entry: {
+    'glycan': [ './js/peaks.js' ],
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  output: {
+    filename: '[name]-bundle.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+    {
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: [
+            ['@babel/preset-env', {
+              modules: false,
+              corejs: 'core-js@2',
+              useBuiltIns: 'entry',
+              targets: {
+                browsers: [
+                  'Chrome >= 60',
+                  'Safari >= 10.1',
+                  'iOS >= 10.3',
+                  'Firefox >= 54',
+                  'Edge >= 15',
+                ],
+              },
+            }],
+          ],
+        },
+      },
+    }],
+  },
+};
